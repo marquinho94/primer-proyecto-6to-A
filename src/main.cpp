69199fb97fba08d0c17cff1d3878e3d32a5bf6fe
marquinho94 <marcoalanmiranda@gmail.com>
2025-05-05 13:59:54 -0300

    proyecto base compilado

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Serial.println("setup");
+}
+
+
+void loop() {
+  digitalWrite(14, HIGH); // turn the LED on (HIGH is the voltage level)
+  Serial.println("Led_on");
+  delay(1000); // wait for a second
+  digitalWrite(14, LOW); 
+  Serial.println("Led_off");
+  delay(1000); // wait for a second
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
